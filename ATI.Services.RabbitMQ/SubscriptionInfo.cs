75a1dd803f7b5fb7ef7ffbee1de25856781429ee
Александр Понизов <aleksandr.p@ati.su>
2024-07-19 13:22:01 +0300

    fix modifier for change tracking

--- ATI.Services.RabbitMQ/SubscriptionInfo.cs
+++ ATI.Services.RabbitMQ/SubscriptionInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// ReSharper disable PropertyCanBeMadeInitOnly.Global
+using System;
 using System.Threading.Tasks;
 using EasyNetQ;
 using EasyNetQ.Consumer;
@@ -7,11 +8,11 @@ namespace ATI.Services.RabbitMQ;
 
 public class SubscriptionInfo
 {
-    public required QueueExchangeBinding Binding { get; init; }
+    public required QueueExchangeBinding Binding { get; set; }
 
-    public required Func<byte[], MessageProperties, MessageReceivedInfo, Task<AckStrategy>> EventbusSubscriptionHandler { get; init; }
+    public required Func<byte[], MessageProperties, MessageReceivedInfo, Task<AckStrategy>> EventbusSubscriptionHandler { get; set; }
 
-    public string? MetricsEntity { get; init; }
+    public string? MetricsEntity { get; set; }
     public required IDisposable Consumer { get; set; }
 
     public Task? ResubscribeTask { get; set; }
